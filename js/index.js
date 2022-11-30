9c80e40e80143009728e6f85d303cbffae838342
lizavr <elizavetarabetskaya@gmail.com>
2022-11-30 17:53:25 +0300

    added projects

--- js/index.js
+++ js/index.js
@@ -10,3 +10,16 @@ function myFunction() {
     arrow.style.transform = "rotate(135deg)";
   }
 }
+
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
